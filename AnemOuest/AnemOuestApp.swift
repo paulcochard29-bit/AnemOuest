dab7a7f9c285b678d81afdbe7ce59a5c6d81474d
paulcochard29-bit <paul.cochard29@gmail.com>
2025-12-28 10:58:36 +0100

    Initial Commit

---
+++ AnemOuest/AnemOuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnemOuestApp.swift
+//  AnemOuest
+//
+//  Created by Paul Cochard on 28/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnemOuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
